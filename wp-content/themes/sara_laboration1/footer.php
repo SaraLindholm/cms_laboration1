f439a86ab9de439bff39a7417eb68a3259b7e6ae
SaraLindholm <sara.lindholm88@gmail.com>
2024-12-02 13:46:19 +0100

    installerade form och la till i kontakt

---
+++ wp-content/themes/sara_laboration1/footer.php
@@ -0,0 +1,10 @@
+<?php
+include 'helper.php'; ?>
+
+<footer>
+  <?php
+  echo "Hallå jag är en footer" ?>
+</footer>
+</body>
+
+</html>
---
+++ wp-content/themes/sara_laboration1/front-page.php
@@ -0,0 +1,18 @@
+<?php
+include 'helper.php';
+get_header();
+?>
+
+<?php wp_body_open() ?>
+
+
+<p>front-pagemall</p>
+<?php
+while (have_posts()) {
+  the_post();
+  the_title();
+  the_content();
+  the_post_thumbnail();
+} ?>
+
+<?php get_footer() ?>
