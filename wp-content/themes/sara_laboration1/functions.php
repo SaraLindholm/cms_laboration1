53dfb30c23c92ca7d274bd0776d674d2911a0210
SaraLindholm <sara.lindholm88@gmail.com>
2024-12-03 14:19:10 +0100

    lagt til funktion för att få tillgång till fonts och javascript

--- wp-content/themes/sara_laboration1/functions.php
+++ wp-content/themes/sara_laboration1/functions.php
@@ -50,14 +50,12 @@ function my_theme_style()
 add_action('wp_enqueue_scripts', 'my_theme_style');
 
 
-// function my_theme_styles()
-// {
-//   wp_enqueue_style('sara_laboration1', get_stylesheet_uri());
-// }
-// add_action('wp_enqueue_scripts', 'my_theme_styles');
-
-
-// function minStyleFunktion()
-// {
-//   wp_enqueue_style('style', get_template_directory_uri() . '/css' . '/style.css');
-// }
+function my_theme_enqueue_styles()
+{
+  wp_enqueue_style('font-awesome', get_template_directory_uri() . '/css/font-awesome.css');
+  wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.css');
+  wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css');
+  wp_enqueue_script('jquery');
+  wp_enqueue_script('custom-script', get_template_directory_uri() . '/js/script.js', array('jquery'), null, true);
+}
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
