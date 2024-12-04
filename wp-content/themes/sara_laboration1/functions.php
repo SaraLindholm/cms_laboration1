9c67c95a30e4dc56a0d14cc95a4f0c0c104bd1c2
SaraLindholm <sara.lindholm88@gmail.com>
2024-12-04 10:03:13 +0100

    bytt ut till en godknt funktion för att visa aktuell författare. ska nu försöka lista ut hur jag styr Vart den länken pekar och vilken som är dess mall

--- wp-content/themes/sara_laboration1/functions.php
+++ wp-content/themes/sara_laboration1/functions.php
@@ -39,6 +39,7 @@ function my_theme_setup()
 {
   add_theme_support('post-thumbnails');
   add_theme_support('menus');
+  add_theme_support('title-tag');
 }
 add_action('after_setup_theme', 'my_theme_setup'); //För att invänta rätt plats i produktionen
 
