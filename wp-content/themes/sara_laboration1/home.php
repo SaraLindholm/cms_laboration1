7905ea601e405367dc8b524fa0168ecf65334b5c
SaraLindholm <sara.lindholm88@gmail.com>
2024-12-06 15:23:38 +0100

    la dynamiska widgets i en sidebar för att få det lite mer överskådligt.

--- wp-content/themes/sara_laboration1/home.php
+++ wp-content/themes/sara_laboration1/home.php
@@ -54,33 +54,7 @@ get_header();
         </div>
 
         <aside id="secondary" class="col-xs-12 col-md-3">
-          <div id="sidebar">
-
-            <ul>
-              <li id="search-2" class="widget widget_search">
-                <?php dynamic_sidebar('aside-sok'); ?>
-              </li>
-            </ul>
-            <ul role="navigation">
-
-              <?php dynamic_sidebar('aside-sidor'); ?>
-
-              <!-- ARKIV -->
-              <li id="archives-2" class="widget widget_archive">
-
-
-                <?php dynamic_sidebar('aside-arkiv') ?>
-
-              </li>
-              <!-- Kategorier -->
-              <li id="categories-2" class="widget widget_categories">
-
-                <?php dynamic_sidebar('aside-kategori') ?>
-
-
-              </li>
-            </ul>
-          </div>
+          <?php get_sidebar(); ?>
         </aside>
 
       </div>
